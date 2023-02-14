eff15f2db96ce88843a635a5f9894ab658b64fc0
dmr-911 <dewan.mizanur911@gmail.com>
2023-02-15 03:24:16 +0600

    register page implemented design with sass

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Add from "../images/addAvatar.png";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="form-container">
+      <div className="form-wrapper">
+        <span className="logo">Chat app</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="text" name="" id="" placeholder="Enter your name" />
+          <input type="email" name="" id="" placeholder="Enter your email" />
+          <input type="password" name="" id="" placeholder="Enter password" />
+          <label htmlFor="image">
+            <img src={Add} alt="" />
+            <span>Add an avatar</span>
+          </label>
+          <input type="file" name="" id="image" style={{ display: "none" }} />
+          <button type="submit">Sign up</button>
+        </form>
+        <p>
+          You have an account? <span>Login</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
