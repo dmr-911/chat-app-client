b800d740127403aaae7ad946b4c3483c00238533
dmr-911 <dewan.mizanur911@gmail.com>
2023-02-16 00:37:36 +0600

    navbar updated with user information

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
 import { signOut } from "firebase/auth";
 import React from "react";
+import { useData } from "../context/AuthContextProvider";
 import { auth } from "../firebase";
 import { ONLINE_IMAGE } from "../utils/data";
 
 const Navbar = () => {
+  const { user } = useData();
+
   return (
     <div className="navbar">
       <span className="logo">Logo</span>
       <div className="user">
         <img src={ONLINE_IMAGE} alt="" />
-        <span>User</span>
+        <span>{user.displayName}</span>
         <button onClick={() => signOut(auth)}>Logout</button>
       </div>
     </div>
