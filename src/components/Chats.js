71b7b7342d2ddca690299760d0dd6aec34d2a207
dmr-911 <dewan.mizanur911@gmail.com>
2023-02-15 15:27:00 +0600

    basic sidebar design completed

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,9 +1,90 @@
-import React from 'react'
+import React from "react";
 
 const Chats = () => {
   return (
-    <div>Chats</div>
-  )
-}
+    <div className="chats">
+      <div className="user-chat">
+        <img
+          src="https://images.pexels.com/photos/14589717/pexels-photo-14589717.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+          alt=""
+        />
+        <div className="user-chat-info">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="user-chat">
+        <img
+          src="https://images.pexels.com/photos/14589717/pexels-photo-14589717.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+          alt=""
+        />
+        <div className="user-chat-info">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="user-chat">
+        <img
+          src="https://images.pexels.com/photos/14589717/pexels-photo-14589717.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+          alt=""
+        />
+        <div className="user-chat-info">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="user-chat">
+        <img
+          src="https://images.pexels.com/photos/14589717/pexels-photo-14589717.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+          alt=""
+        />
+        <div className="user-chat-info">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="user-chat">
+        <img
+          src="https://images.pexels.com/photos/14589717/pexels-photo-14589717.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+          alt=""
+        />
+        <div className="user-chat-info">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="user-chat">
+        <img
+          src="https://images.pexels.com/photos/14589717/pexels-photo-14589717.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+          alt=""
+        />
+        <div className="user-chat-info">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="user-chat">
+        <img
+          src="https://images.pexels.com/photos/14589717/pexels-photo-14589717.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+          alt=""
+        />
+        <div className="user-chat-info">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="user-chat">
+        <img
+          src="https://images.pexels.com/photos/14589717/pexels-photo-14589717.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+          alt=""
+        />
+        <div className="user-chat-info">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Chats
+export default Chats;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className="search">
+      <div className="search-form">
+        <input type="text" name="" id="" placeholder="Find user" />
+      </div>
+      <div className="user-chat">
+        <img
+          src="https://images.pexels.com/photos/14589717/pexels-photo-14589717.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+          alt=""
+        />
+        <div className="user-chat-info">
+          <span>Jane</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
