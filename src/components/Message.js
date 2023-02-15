14ca3ff694f5de686a5b7e227d8c957a3e07f7a5
dmr-911 <dewan.mizanur911@gmail.com>
2023-02-15 16:10:59 +0600

    message container design finished

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Message = () => {
-  return <div className="message">Message</div>;
+  return (
+    <div className="message owner">
+      <div className="message-info">
+        <img
+          src="https://images.pexels.com/photos/14589717/pexels-photo-14589717.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+          alt=""
+        />
+        <span>Just now</span>
+      </div>
+      <div className="message-content">
+        <p>Hello</p>
+        <img
+          src="https://images.pexels.com/photos/14589717/pexels-photo-14589717.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
