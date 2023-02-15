b3d365dbde44cd16717e020fc41d4143af44323e
dmr-911 <dewan.mizanur911@gmail.com>
2023-02-16 00:55:14 +0600

    search user implemeted

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { ONLINE_IMAGE } from "../utils/data";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../firebase";
 
 const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+        setUser(doc.data());
+      });
+    } catch (err) {
+      console.log(err.response);
+      setErr(true);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
   return (
     <div className="search">
       <div className="search-form">
-        <input type="text" name="" id="" placeholder="Find user" />
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Find user"
+          onKeyDown={handleKey}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </div>
-      <div className="user-chat">
-        <img src={ONLINE_IMAGE} alt="" />
-        <div className="user-chat-info">
-          <span>Jane</span>
+      {user && (
+        <div className="user-chat">
+          <img src={user?.photoURL} alt="" />
+          <div className="user-chat-info">
+            <span>{user.displayName}</span>
+          </div>
         </div>
-      </div>
+      )}
+
+      {err && <span>Something went wrong</span>}
     </div>
   );
 };
