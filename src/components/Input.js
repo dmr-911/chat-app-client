d2e708e4d316ed312d3a4bc91dd55de906b587c9
dmr-911 <dewan.mizanur911@gmail.com>
2023-02-15 15:56:00 +0600

    input component updated design

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Img from "../images/img.png";
+import Attach from "../images/attach.png";
 
 const Input = () => {
   return (
-    <div>Input</div>
-  )
-}
+    <div className="input">
+      <input type="text" name="" id="" placeholder="Write something" />
+      <div className="send">
+        <input type="file" style={{ display: "none" }} id="file" />
 
-export default Input
+        <label htmlFor="file">
+          <img src={Attach} alt="" />
+        </label>
+        <img src={Img} alt="" />
+        <button>Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default Input;
