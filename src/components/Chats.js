1a29b7a35bb1e798faaa698d337d05552063dbd3
dmr-911 <dewan.mizanur911@gmail.com>
2023-02-16 15:49:33 +0600

    chat component updated with realitme get database of firebase

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,44 +1,46 @@
-import React from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useData } from "../context/AuthContextProvider";
+import { db } from "../firebase";
 import { ONLINE_IMAGE } from "../utils/data";
 
 const Chats = () => {
+  const [chats, setChats] = useState([]);
+
+  const { user } = useData();
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", user.uid), (doc) => {
+        // console.log("Current data: ", doc.data());
+        setChats(doc.data());
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+
+    user.uid && getChats();
+  }, [user.uid]);
+
+  console.log(Object.entries(chats));
+
   return (
     <div className="chats">
-      <div className="user-chat">
-        <img src={ONLINE_IMAGE} alt="" />
-        <div className="user-chat-info">
-          <span>Jane</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="user-chat">
-        <img src={ONLINE_IMAGE} alt="" />
-        <div className="user-chat-info">
-          <span>Jane</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="user-chat">
-        <img src={ONLINE_IMAGE} alt="" />
-        <div className="user-chat-info">
-          <span>Jane</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="user-chat">
-        <img src={ONLINE_IMAGE} alt="" />
-        <div className="user-chat-info">
-          <span>Jane</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="user-chat">
-        <img src={ONLINE_IMAGE} alt="" />
-        <div className="user-chat-info">
-          <span>Jane</span>
-          <p>Hello</p>
-        </div>
-      </div>
+      {Object.entries(chats)?.map((chat) => {
+        console.log(chat);
+        const { displayName, photoURL, lastMessage } = chat[1].userInfo;
+        return (
+          <div className="user-chat" key={chat[0]}>
+            <img src={photoURL} alt="" />
+            <div className="user-chat-info">
+              <span>{displayName}</span>
+              <p>{lastMessage?.text}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
